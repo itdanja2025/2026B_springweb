600d3750143ca3c2715037260a459b6cd7b7cb1b
itdanja2025 <itdanja2025@gmail.com>
2026-08-26 10:24:31 +0900

    day36 1h

---
+++ src/main/java/example/day02/model/dao/BoardDao.java
@@ -0,0 +1,7 @@
+package example.day02.model.dao;
+
+public class BoardDao extends BaseDao {
+    private BoardDao(){};
+    private static final BoardDao instance = new BoardDao();
+    public static BoardDao getInstance( ){ return instance; }
+}
