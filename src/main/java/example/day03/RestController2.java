cfca0cc5b2700b38af5fd1e4ef7192c78c23cd92
itdanja2025 <itdanja2025@gmail.com>
2026-08-31 17:09:00 +0900

    day39 7h

--- src/main/java/example/day03/RestController2.java
+++ src/main/java/example/day03/RestController2.java
@@ -7,6 +7,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -47,9 +50,37 @@ public class RestController2 {
     }
     // 5.http://localhost:8080/day03/task9?name=유재석&age=10
     @DeleteMapping("/task9")
-    public int task9( @ModelAttribute ExamDto examDto ){ // 
+    public int task9( @ModelAttribute ExamDto examDto ){
         System.out.println( examDto );
         return 9;
     }
+    // 6. http://localhost:8080/day03/task10/유재석/10
+    @GetMapping("/task10/{name}/{age}")
+    public int task10( 
+        @PathVariable( name = "name") String name , 
+        @PathVariable( name ="age") int age ){
+        System.out.println( name ); 
+        System.out.println( age );
+        return 10;
+    }
+    // 7. http://localhost:8080/day03/task11 , { "name" : "유재석" , "age" : 10 }
+    @PostMapping("/task11")
+    public int task11( @RequestBody ExamDto examDto ){
+        System.out.println( examDto );
+        return 11;
+    }
 
 } // CLASS END 
+/*
+    요청Request ----------->  
+                   HTTP
+            <--------------- 응답Response
+    * HTTP content-type : 전송하는 데이터의 타입 뜻
+    쿼리스트링이란? URL?매개변수명=값&매개변수명=값
+    경로구분자이란? URL/값1/값2
+    1. @RequestParam : 쿼리스트링 또는 content-tpye:'application/x-www-form-urlencoded'(기본값)
+    2. @ModelAttribute(생략가능) : + DTO 매핑
+    3. @PathVariable : 경로구분자
+    4. @RequestBody : content-tpye:'application/json'
+    -> POST / PUT 메소드만 body(본문-경로상의값노출이없다) 지원한다. 
+*/
