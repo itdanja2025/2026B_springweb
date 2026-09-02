ab671ed364cf364f4643189334c5f69514335fd4
itdanja2025 <itdanja2025@gmail.com>
2026-09-02 15:43:02 +0900

    day40 5h

--- src/main/java/example/day04/ExamController.java
+++ src/main/java/example/day04/ExamController.java
@@ -2,9 +2,12 @@ package example.day04;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
@@ -26,4 +29,17 @@ public class ExamController {
         // 서비스 호출
         return examService.저장( entity );
     }
-}
+    // [3] 삭제 , http://localhost:8080/day04/exam?no=3
+    @DeleteMapping("/day04/exam")
+    public boolean 삭제( @RequestParam( name = "no") int no ){
+        // 서비스호출
+        return examService.삭제( no );
+    }
+    // [4] 수정 , http://localhost:8080/day04/exam , { "eno" : "1" , "ename" : "김현수"}
+    @PutMapping( "/day04/exam" )
+    public boolean 수정( @RequestBody ExamEntity entity ){
+        // 서비스호출
+        return examService.수정( entity );
+    }
+
+} // class end 
